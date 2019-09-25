f3ec7d0725779af6396fee0951363bb9f9119142
KaranParwani1116 <karanparwani.parwani102@gmail.com>
2019-09-25 18:16:58 +0530

    Image Slider Added

--- app/src/main/java/com/example/hospiton/Destination.java
+++ app/src/main/java/com/example/hospiton/Destination.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -37,7 +41,13 @@ import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -51,17 +61,19 @@ public class Destination extends AppCompatActivity {
     AutocompleteSupportFragment places_fragment;
     private String place_id="";
     private ImageView placephoto;
+    List<PhotoMetadata>allphotos;
     private Button Find;
+    private SliderView sliderView;
+    private List<Uri>ImageUri;
+    private SliderAdapterExample adapter;
+    private String TAG=Destination.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
 
-        placephoto=(ImageView)findViewById(R.id.photo);
-        Find=(Button)findViewById(R.id.find_photo);
-
-
+        ImageUri=new ArrayList<>();
         requestpermission();
         setupPlacesAutocomplete();
 
@@ -69,20 +81,18 @@ public class Destination extends AppCompatActivity {
 
         getCurrentPlace();
 
-        Find.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(TextUtils.isEmpty(place_id))
-                {
-                    Toast.makeText(Destination.this,"Must not be null",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                else{
-                    getPhotoandDetail(place_id);
-                }
-            }
-        });
+        sliderView = findViewById(R.id.imageSlider);
+        adapter = new SliderAdapterExample();
 
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        sliderView.startAutoCycle();
 
     }
 
@@ -91,23 +101,31 @@ public class Destination extends AppCompatActivity {
          placesClient.fetchPlace(request).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
              @Override
              public void onSuccess(FetchPlaceResponse fetchPlaceResponse) {
-                 Place place=fetchPlaceResponse.getPlace();
+                 Place place = fetchPlaceResponse.getPlace();
 
-                 PhotoMetadata photoMetadata=place.getPhotoMetadatas().get(0);
+                 allphotos = place.getPhotoMetadatas();
+                 Log.d(TAG, String.valueOf(allphotos.size()));
 
-                 FetchPhotoRequest photoRequest=FetchPhotoRequest.builder(photoMetadata).build();
-                 placesClient.fetchPhoto(photoRequest).addOnSuccessListener(new OnSuccessListener<FetchPhotoResponse>() {
-                     @Override
-                     public void onSuccess(FetchPhotoResponse fetchPhotoResponse) {
-                         Bitmap bitmap=fetchPhotoResponse.getBitmap();
-                         placephoto.setImageBitmap(bitmap);
-                     }
-                 }).addOnFailureListener(new OnFailureListener() {
-                     @Override
-                     public void onFailure(@NonNull Exception e) {
-                         Toast.makeText(Destination.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-                     }
-                 });
+                  if(allphotos!=null) {
+                      for (int i = 0; i < allphotos.size(); i++) {
+                          FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(allphotos.get(i)).build();
+                          placesClient.fetchPhoto(photoRequest).addOnSuccessListener(new OnSuccessListener<FetchPhotoResponse>() {
+                              @Override
+                              public void onSuccess(FetchPhotoResponse fetchPhotoResponse) {
+                                  Bitmap bitmap = fetchPhotoResponse.getBitmap();
+                                  Uri uri = getImageUri(Destination.this, bitmap);
+                                  ImageUri.add(uri);
+                              }
+                          }).addOnFailureListener(new OnFailureListener() {
+                              @Override
+                              public void onFailure(@NonNull Exception e) {
+                                  Toast.makeText(Destination.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                              }
+                          });
+                      }
+                      adapter.datachanged(ImageUri);
+                      ImageUri.clear();
+                  }
              }
          }).addOnFailureListener(new OnFailureListener() {
              @Override
@@ -121,7 +139,8 @@ public class Destination extends AppCompatActivity {
        Dexter.withActivity(this)
                .withPermissions(
                        Manifest.permission.ACCESS_FINE_LOCATION,
-                       Manifest.permission.ACCESS_COARSE_LOCATION
+                       Manifest.permission.ACCESS_COARSE_LOCATION,
+                       Manifest.permission.WRITE_EXTERNAL_STORAGE
                ).withListener(new MultiplePermissionsListener() {
            @Override
            public void onPermissionsChecked(MultiplePermissionsReport report) {
@@ -133,6 +152,12 @@ public class Destination extends AppCompatActivity {
                   Toast.makeText(Destination.this,"You must enable this permission",Toast.LENGTH_SHORT).show();
            }
        }).check();
+    }
+    private Uri getImageUri(Context context, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 
     private void getCurrentPlace() {
@@ -157,7 +182,7 @@ public class Destination extends AppCompatActivity {
                 Collections.reverse(response.getPlaceLikelihoods());
                 place_id=response.getPlaceLikelihoods().get(0).getPlace().getId();
 
-                Toast.makeText(Destination.this,response.getPlaceLikelihoods().get(0).getPlace().getAddress(),Toast.LENGTH_LONG).show();
+                Toast.makeText(Destination.this,response.getPlaceLikelihoods().get(0).getPlace().getAddress()+response.getPlaceLikelihoods().get(0).getPlace().getLatLng(),Toast.LENGTH_LONG).show();
 
             }
         }).addOnFailureListener(new OnFailureListener() {
